21139cc35ddc5ff1498e5175586f0deff27aaa34
Teagan Clayton <clay0063@algonquinlive.com>
2023-10-20 22:03:07 -0400

    Fix most of Add Ideas page issues

--- screens/AddIdeasScreen.js
+++ screens/AddIdeasScreen.js
@@ -1,7 +1,13 @@
-import { Text, TextInput, useTheme } from "react-native-paper"
-import { SafeAreaView } from "react-native-safe-area-context"
-import { View, StyleSheet, Image, KeyboardAvoidingView } from "react-native"
-import { useState } from "react"
+import { Text, TextInput, useTheme } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  View,
+  StyleSheet,
+  Image,
+  KeyboardAvoidingView,
+  ScrollView,
+} from "react-native";
+import { useState } from "react";
 import { useList } from "../context/ListContext";
 import ErrorModal from "../components/ErrorModal";
 import UseCamera from "../components/UseCamera";
@@ -9,8 +15,8 @@ import CancelButton from "../components/CancelButton";
 import SaveButton from "../components/SaveButton";
 import bundleIdeaData from "../utils/bundleIdeaData";
 
-const AddIdeasScreen = ({navigation, route}) => {
-  const {addItemByID} = useList();
+const AddIdeasScreen = ({ navigation, route }) => {
+  const { addItemByID } = useList();
   const theme = useTheme();
 
   const [errorMessage, setErrorMessage] = useState("");
@@ -32,12 +38,12 @@ const AddIdeasScreen = ({navigation, route}) => {
       const data = bundleIdeaData(text, image);
       try {
         await addItemByID(id, data);
-        navigation.navigate("Idea List", {uid: id})
+        navigation.navigate("Idea List", { uid: id });
       } catch (error) {
         showModal(error.message);
       }
-    } else { 
-      showModal("You must have both an idea name and picture.")
+    } else {
+      showModal("You must have both an idea name and picture.");
     }
   };
 
@@ -46,52 +52,63 @@ const AddIdeasScreen = ({navigation, route}) => {
   };
 
   return (
-    <SafeAreaView style={{flex:1}}>
+    <SafeAreaView style={{flex: 1}}>
+        <View style={styles.container}>
+            <Text
+              style={{ color: theme.colors.primary, textAlign: "center" }}
+              variant="titleMedium"
+            >
+              Enter your Idea
+            </Text>
+            <TextInput
+              placeholder="Idea"
+              value={text}
+              onChangeText={(text) => setText(text)}
+              style={{ width: "100%" }}
+              backgroundColor={theme.colors.secondaryContainer}
+            />
 
-      <View style={styles.container}>
+            <Text
+              style={{ color: theme.colors.primary, textAlign: "center" }}
+              variant="titleMedium"
+            >
+              Take a Picture
+            </Text>
+            <UseCamera onPhotoTaken={handlePhotoTaken} />
 
-        <KeyboardAvoidingView
-            behavior={Platform.OS === "ios" ? "padding" : "height"}
-            style={{ flex: 1, width: "100%" }} >
-          <Text style={{ color: theme.colors.primary, textAlign: "center" }} variant="titleMedium">Enter your Idea</Text>
-          <TextInput
-            placeholder="Idea"
-            value={text}
-            onChangeText={text => setText(text)}
-            style={{width:"100%"}}
-            backgroundColor={theme.colors.secondaryContainer}
-          />
-        </KeyboardAvoidingView>
-        
-        <Text style={{ color: theme.colors.primary }} variant="titleMedium">Take a Picture</Text>
-        <UseCamera onPhotoTaken={handlePhotoTaken} />
+            <View style={{margin:10}}>
+              {image ? (
+                <Image
+                  source={{ uri: image.uri }}
+                  width={image.width}
+                  height={image.height}
+                ></Image>
+              ) : (
+                <Text>Take an image!</Text>
+              )}
+            </View>
 
-        <View>
-          <Text>Image View</Text>
-          {image ? (
-            <Image source={{uri:image.uri}} width={image.width} height={image.height}></Image>
-          ) : (
-            <Text>Take an image!</Text>
-          )}
+          <View style={{ flexDirection: "row", marginVertical: 10 }}>
+            <CancelButton
+              onPress={() => navigation.navigate("Idea List", { uid: id })}
+            />
+            <SaveButton
+              text={text}
+              data={image}
+              onPress={() => handleSaveData()}
+            />
+          </View>
         </View>
-
-        <View style={{flexDirection: "row", marginVertical:10}}>
-          <CancelButton onPress={() => navigation.navigate("Idea List", {uid: id})} />
-          <SaveButton text={text} data={image} onPress={() => handleSaveData()} />
-        </View>
-      </View>
-
       <ErrorModal errorMessage={errorMessage} clearError={clearError} />
-
     </SafeAreaView>
-  )
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-between",
     padding: 4,
   },
 });
